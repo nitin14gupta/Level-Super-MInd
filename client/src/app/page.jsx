07a03842cdbeba212e9275a9d19ef2a62d76e92c
Rehbar Khan <136853370+thisisarsh1@users.noreply.github.com>
2025-01-18 23:44:50 +0530

    Bestperforming Hooks

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Landing from "@/components/Landing";
 import Competitors from "@/components/Competitors";
 import SatisfactoryAnalysis from "@/components/SatisfactoryAnalysis";
+import BestHooks from "@/components/BestHooks";
 export default function Home() {
   return (
     <div>
       Hello, world!
       <Landing />
       <Competitors />
+      <BestHooks></BestHooks>
       <SatisfactoryAnalysis />
     </div>
   );
