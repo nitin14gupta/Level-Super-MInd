73d105152f8d9b7983fcd816d8c215ed1d4f8c25
Fareed95 <fareedsayed95@gmail.com>
2025-01-21 03:18:08 +0530

    donw dirrectly from json

---
+++ models/overall_flow/app.py
@@ -0,0 +1,23 @@
+from langflow.load import run_flow_from_json
+import warnings
+import logging
+import json
+
+warnings.filterwarnings("ignore")  # Suppress warnings
+logging.getLogger("langchain").setLevel(logging.ERROR)  # Suppress langchain logs
+
+description = "i want to start an education business"
+industry = "education"
+TWEAKS = {
+  "TextInput-6hSnG": {"input_value":description},
+  "TextInput-KwOQK": {"input_value":industry}
+}
+
+result = run_flow_from_json(flow="ART GENERATOR.json",
+                            input_value="message",
+                            session_id="", # provide a session id if you want to use session state
+                            fallback_to_env_vars=False, # False by default
+                            tweaks=TWEAKS)
+# Assuming result contains the output returned from the flow
+message = result[0].outputs[2].results['text']
+print(message)
